27e59e8bc6600b058c0de29ff24121b853a30773
lk90 <lokesh.thakur90@gmail.com>
2020-10-20 17:54:33 -0500

    Add files via upload

--- cnn.py
+++ cnn.py
@@ -1,118 +1,57 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Nov 15 23:00:57 2019
-
-@author: lokeshkumar
-"""
-
-
-from __future__ import absolute_import, division, print_function, unicode_literals
-
-import tensorflow as tf
-import numpy as np
-
-from tensorflow.keras import datasets, layers, models
 from tensorflow import keras
+#import tensorflow as tf
+
+from tensorflow.keras.datasets import cifar10
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers import Dropout
-#from keras.layers.normalization import BatchNormalization
-import matplotlib.pyplot as plt
-from time import time 
-import os,datetime
-from tensorflow.python.keras.callbacks import TensorBoard
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.constraints import max_norm
+from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import MaxPooling2D
+# load data
+(X_train, y_train), (X_test, y_test) = cifar10.load_data()
 
-# Load the TensorBoard notebook extension
-#%load_ext tensorboard
+# normalize inputs
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+X_train = X_train / 255.0
+X_test = X_test / 255.0
 
-
-print(tf.__version__)
-
-(train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
-
-class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
-               'dog', 'frog', 'horse', 'ship', 'truck']
-
-print(train_images.shape)
-
-print(len(train_labels))
-
-print(train_labels)
-
-print(test_images.shape)
-
-print(len(test_labels))
-
-plt.figure()
-plt.imshow(train_images[0])
-plt.colorbar()
-plt.grid(False)
-plt.show()
-
-# Normalize pixel values to be between 0 and 1
-train_images = train_images / 255.0
-test_images = test_images / 255.0
-
-plt.figure(figsize=(10,10))
-for i in range(25):
-    plt.subplot(5,5,i+1)
-    plt.xticks([])
-    plt.yticks([])
-    plt.grid(False)
-    plt.imshow(train_images[i], cmap=plt.cm.binary)
-    plt.xlabel(class_names[train_labels[i][0]])
-plt.show()
-train_labels
-
-# Defining model
-model = models.Sequential()
-model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
-#model.add(BatchNormalization())
-model.add(layers.MaxPooling2D((2, 2)))
+# Create the model
+model = Sequential()
+model.add(Conv2D(32, (3, 3), input_shape=(32, 32, 3), activation='relu', padding='same', kernel_constraint=max_norm(3), use_bias = True, bias_initializer='zeros', bias_constraint=max_norm(3) ))
+model.add(MaxPooling2D())
 model.add(Dropout(0.2))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-#model.add(BatchNormalization())
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(Dropout(0.3))
-model.add(layers.Conv2D(128, (3, 3), activation='relu'))
-#model.add(BatchNormalization())
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(Dropout(0.4))
+model.add(Conv2D(64, (3, 3), activation='relu', padding='same', kernel_constraint=max_norm(3), use_bias = True, bias_initializer='zeros', bias_constraint=max_norm(3)))
+model.add(MaxPooling2D())
+model.add(Dropout(0.2))
+model.add(Conv2D(128, (3, 3), activation='relu', padding='same', kernel_constraint=max_norm(3), use_bias = True, bias_initializer='zeros', bias_constraint=max_norm(3)))
+model.add(MaxPooling2D())
+model.add(Dropout(0.2))
+model.add(Flatten())
+model.add(Dropout(0.2))
+model.add(Dense(1024, activation='relu', kernel_constraint=max_norm(3), use_bias = True, bias_initializer='zeros', bias_constraint=max_norm(3)))
+model.add(Dropout(0.2))
+model.add(Dense(512, activation='relu', kernel_constraint=max_norm(3), use_bias = True, bias_initializer='zeros', bias_constraint=max_norm(3)))
+model.add(Dropout(0.2))
+model.add(Dense(256, activation='relu', kernel_constraint=max_norm(3), use_bias = True, bias_initializer='zeros', bias_constraint=max_norm(3)))
+model.add(Dense(10, activation='softmax'))
+# Compile model
+epochs = 30
+lrate = 0.01
 
+model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 model.summary()
 
-model.add(layers.Flatten())
-model.add(layers.Dense(128, activation='relu'))
-#model.add(BatchNormalization())
-model.add(Dropout(0.5))
-#model.add(layers.Dense(64, activation='relu'))
-model.add(layers.Dense(10, activation='softmax'))
+# Fit the model
+model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=epochs, batch_size=64)
+# Final evaluation of the model
+history = model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=epochs, batch_size=64)
 
-model.summary()
-
-#sgd = keras.optimizers.SGD(momentum=0.0, nesterov=False)
-
-model.compile(optimizer='adam',
-              loss='sparse_categorical_crossentropy',
-              metrics=['accuracy'])
-
-tensorboard = TensorBoard(log_dir="logs/fit" .format(time()))
-
-history = model.fit(train_images, train_labels, epochs=11, 
-                    validation_data=(test_images, test_labels),callbacks=[tensorboard])
-#callbacks=[tensorboard]
-
-test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
-
-print('\nTest accuracy: %.3f'%( test_acc*100))
-
-
-predictions = model.predict(test_images)
-
-print(predictions[0])
-
-print(np.argmax(predictions[0]))
-
-print(test_labels[i][0])
+scores = model.evaluate(X_test, y_test, verbose=0)
+print("Accuracy: %.2f%%" % (scores[1]*100))
 
 plt.plot(history.history['acc'], label='accuracy')
 plt.plot(history.history['val_acc'], label = 'val_accuracy')
@@ -120,80 +59,3 @@ plt.xlabel('Epoch')
 plt.ylabel('Accuracy')
 plt.ylim([0.5, 1])
 plt.legend(loc='lower right')
-
-def plot_image(i, predictions_array, true_label, img):
-  predictions_array, true_label, img = predictions_array, true_label[i][0], img[i]
-  plt.grid(False)
-  plt.xticks([])
-  plt.yticks([])
-
-  plt.imshow(img, cmap=plt.cm.binary)
-
-  predicted_label = np.argmax(predictions_array)
-  if predicted_label == true_label:
-    color = 'blue'
-  else:
-    color = 'red'
-
-  plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
-                                100*np.max(predictions_array),
-                                class_names[true_label]),
-                                color=color)
-    
-def plot_value_array(i, predictions_array, true_label):
-  predictions_array, true_label = predictions_array, true_label[i][0]
-  plt.grid(False)
-  plt.xticks(range(10))
-  plt.yticks([])
-  thisplot = plt.bar(range(10), predictions_array, color="#777777")
-  plt.ylim([0, 1])
-  predicted_label = np.argmax(predictions_array)
-
-  thisplot[predicted_label].set_color('red')
-  thisplot[true_label].set_color('blue')
-
-i = 0
-plt.figure(figsize=(6,3))
-plt.subplot(1,2,1)
-plot_image(i, predictions[i], test_labels, test_images)
-plt.subplot(1,2,2)
-plot_value_array(i, predictions[i],  test_labels)
-plt.show()
-
-i = 12
-plt.figure(figsize=(6,3))
-plt.subplot(1,2,1)
-plot_image(i, predictions[i], test_labels, test_images)
-plt.subplot(1,2,2)
-plot_value_array(i, predictions[i],  test_labels)
-plt.show()
-
-num_rows = 5
-num_cols = 3
-num_images = num_rows*num_cols
-plt.figure(figsize=(2*2*num_cols, 2*num_rows))
-for i in range(num_images):
-  plt.subplot(num_rows, 2*num_cols, 2*i+1)
-  plot_image(i, predictions[i], test_labels, test_images)
-  plt.subplot(num_rows, 2*num_cols, 2*i+2)
-  plot_value_array(i, predictions[i], test_labels)
-plt.tight_layout()
-plt.show()
-
-img = test_images[1]
-
-print(img.shape)
-
-img = (np.expand_dims(img,0))
-
-print(img.shape)
-
-predictions_single = model.predict(img)
-
-print(predictions_single)
-
-plot_value_array(1, predictions_single[0], test_labels)
-_ = plt.xticks(range(10), class_names, rotation=45)
-
-print(np.argmax(predictions_single[0]))
-
